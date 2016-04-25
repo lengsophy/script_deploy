f055f1f4c9565c799b023e28bd32e4f6d9082d09
root <root@ip-172-31-7-9.ap-southeast-1.compute.internal>
2016-04-25 10:37:09 +0000

    push newscript

---
+++ scp/dumpbackup/scp_frombackdump.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo apt-get install zip
+wait
+sudo apt-get install unzip
+wait
+sudo chmod 777 linux-class.pem
+wait
+sudo chmod 777 .
+wait
+date=$(date +"%m_%d_%Y")
+scp -i linux-class.pem ubuntu@54.173.194.74:/home/ubuntu/safir_dum_backup/$date/dump.zip .
+wait
+unzip dump.zip
+wait
+sudo rm dump.zip
+echo "Finish"
