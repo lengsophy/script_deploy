6247fa7e0ab1c99633e0722ca400464e381bf0cd
Ubuntu <ubuntu@ip-172-31-19-41.ap-southeast-1.compute.internal>
2016-04-27 10:00:17 +0000

    update

--- scp/cdnscp/scp_cdn_amazon.sh
+++ scp/cdnscp/scp_cdn_amazon.sh
@@ -7,7 +7,7 @@ sudo chmod 777 Server-keypair.pem
 wait
 sudo chmod 777 .
 wait
-scp -i Server-keypair.pem ubuntu@52.221.226.42:/home/ubuntu/publice.zip .
+sudo -u nobody scp -i Server-keypair.pem ubuntu@52.221.226.42:/home/ubuntu/publice.zip .
 wait
 unzip publice.zip
 wait
@@ -15,6 +15,6 @@ sudo rm publice.zip
 wait
 sudo rm /var/www/html/* -r
 wait 
-sudo cp -R /public/public/* /var/www/html/
+sudo cp -R public/public/* /var/www/html/
 
 echo "Finish"
